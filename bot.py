b91388f916fa94a51bb25d1d08166112e6057059
karim <kawzaknobo515151@gmail.com>
2026-09-17 10:33:03 +0300

    gfdgfdg

--- bot.py
+++ bot.py
@@ -1537,7 +1537,7 @@ def Multi_loop():
                 Res_File = Media_Skip(File)
               Upld_File(Res_File,File_Msg)
 
-         elif process in ('Renm') : 
+         elif process in ('Renm',) : 
            if process == 'Renm':
               Ext = File.split('.')[-1]
               Res_File = f"{dl_path}{Rate.replace('|',' ')}.{Ext}"
